66595ac35a46cbf0ef1a05fcded02779379e6a1c
anujpatidar2005 <anujpatidar2005@gmail.com>
2026-05-21 15:08:01 +0530

    Delete mystreamlit directory

--- mystreamlit/app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("My First Streamlit App 🙋‍♂️")
-st.write("Hello, welcome to Streamlit 🚀")
-
-name = st.text_input("Enter your name")
-
-if name:
-    st.success(f"Hello {name} 👋")
